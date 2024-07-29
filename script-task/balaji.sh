fe134f3c50a5508d809ad4482291bf02a0bc0407
bswamynathan <balaji.cs97@gmail.com>
2024-07-29 23:03:17 +0530

    script-task

---
+++ script-task/balaji.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir marvel-studios
+cd marvel-studios
+echo "Directory marvel-studios is created"
+pwd
+touch ironman.txt
+touch spiderman.txt
+echo "love you 3000" > ironman.txt
+echo "Hi, I am PeterParker" > spiderman.txt
+ls -ltr *.txt
+echo "Done"
+
+
